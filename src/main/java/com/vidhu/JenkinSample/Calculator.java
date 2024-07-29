331655e8e4e6a8901e820c56f2dbd17af0ffa41b
vidhusha18 <115626945+vidhusha18@users.noreply.github.com>
2024-07-29 09:54:25 +0530

    Add files via upload

---
+++ src/main/java/com/vidhu/JenkinSample/Calculator.java
@@ -0,0 +1,17 @@
+package com.vidhu.JenkinSample;
+
+public class Calculator {
+
+	public static int add(int a , int b) {
+		int sum=a+b;
+		return sum;
+		
+	}
+	
+	public static int subtract(int a , int b) {
+		int sub=a-b;
+		return sub;
+		
+	}
+	
+}
